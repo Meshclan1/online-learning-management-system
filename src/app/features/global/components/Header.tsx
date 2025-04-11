6934adb5da6ca03348de80b58e301b156c2f9b42
Ayo <Ayo.meshe@gmail.com>
2025-04-11 17:47:28 +0100

    route structure

--- src/app/features/global/components/Header.tsx
+++ src/app/features/global/components/Header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <header className="border bottom-1">
       <nav className="bg-white border-gray-200 px-4 py-2.5">
-        <div className="flex justify-center items-center mx-auto max-w-screen-xl">
+        <div className="flex flex-wrap justify-center items-center mx-auto max-w-screen-xl">
           <div className="flex items-center">
             <Link href="/" className="flex items-center">
               <Button variant="ghost" className="w-32 text-2xl">
@@ -15,13 +15,21 @@ const Header = () => {
               </Button>
             </Link>
           </div>
-          <div className="flex items-center">
-            <Link href="/courses" className="flex items-center">
-              <Button variant="ghost" className="w-32 text-2xl">
-                Courses
-              </Button>
-            </Link>
-          </div>
+          <Link href="/courses" className="flex items-center">
+            <Button variant="ghost" className="w-32 text-2xl">
+              Courses
+            </Button>
+          </Link>
+          <Link href="/dashboard" className="flex items-center">
+            <Button variant="ghost" className="w-32 text-2xl">
+              Dashboard
+            </Button>
+          </Link>
+          <Link href="/admin" className="flex items-center">
+            <Button variant="ghost" className="w-32 text-2xl">
+              Admin
+            </Button>
+          </Link>
         </div>
       </nav>
     </header>
