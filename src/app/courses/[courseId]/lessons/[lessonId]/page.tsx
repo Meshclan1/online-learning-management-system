b38265cc58654a8ae16cf197530546018730a6b6
Ayo <Ayo.meshe@gmail.com>
2025-04-16 15:15:01 +0100

    implementing dynamic routes

--- src/app/courses/[courseId]/lessons/[lessonId]/page.tsx
+++ src/app/courses/[courseId]/lessons/[lessonId]/page.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 
-const LessonIdPage = () => {
+const LessonIdPage = async ({
+  params,
+}: {
+  params: Promise<{ lessonId: string }>;
+}) => {
+  const { lessonId } = await params;
+
   return (
     <div className="flex h-screen items-center justify-center bg-grey-100">
       <h1 className="text-3xl p-4 border-4 text-orange-400 bg-white">
-        This is a LessonID Page{" "}
+        This is a LessonID Page for {lessonId}
       </h1>
     </div>
   );
--- src/app/courses/[courseId]/page.tsx
+++ src/app/courses/[courseId]/page.tsx
@@ -1,10 +1,30 @@
 import React from "react";
 
-const CourseIdPage = () => {
+// export default async function CourseIdPage({
+//   params: { courseId },
+// }: {
+//   params: { courseId: string };
+// }) {
+//   return (
+//     <div className="flex h-screen items-center justify-center bg-grey-100">
+//       <h1 className="text-3xl p-4 border-4 text-orange-400 bg-white">
+//         This is a COURSE ID Page of {courseId}
+//       </h1>
+//     </div>
+//   );
+// }
+
+const CourseIdPage = async ({
+  params,
+}: {
+  params: Promise<{ courseId: string }>;
+}) => {
+  const { courseId } = await params;
+
   return (
     <div className="flex h-screen items-center justify-center bg-grey-100">
       <h1 className="text-3xl p-4 border-4 text-orange-400 bg-white">
-        This is a COURSE ID Page{" "}
+        This is a COURSE ID Page for - {courseId}
       </h1>
     </div>
   );
